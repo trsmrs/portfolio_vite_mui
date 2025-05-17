c129e674363868faee2d174391c6ffa6e23d5e08
tiago <trsminfo@gmail.com>
2025-05-17 15:19:14 -0300

    ajustando tamanho da view height

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -14,8 +14,8 @@ export const Hero = () => {
             <Container maxWidth="lg">
                 <Grid container spacing={2}>
                     <Grid size={{ xs: 12, md: 5 }}>
-                        <Box position="relative">
-                            <Box position="absolute" width={"150%"} top={-100} right={-150}>
+                        <Box position="relative" pb={3}>
+                            <Box width={"150%"} position="absolute" top={-100} right={-150}>
                                 <AnimatedBackground />
                             </Box>
                             <Box textAlign="center" position='relative'>
@@ -26,23 +26,27 @@ export const Hero = () => {
                     <Grid size={{ xs: 12, md: 7 }}>
                         <Typography color="primary.contrastText" variant="h1" textAlign={"center"}>Tiago Machado</Typography>
                         <Typography color="primary.contrastText" variant="h2" textAlign={"center"}>Software Engineer</Typography>
-                        <Grid container rowSpacing={1} columnSpacing={{ xs: 2, md: 4 }} display={"flex"} justifyContent={"center"}>
-                            <Grid size={{ xs: 12, md: 4 }} display={"flex"} justifyContent={"center"}>
-                                <Button>
-                                    <DownloadIcon />
-                                    <Typography>Download CV</Typography>
-                                </Button>
-                            </Grid>
-                            <Grid size={{ xs: 12, md: 4 }} display={"flex"} justifyContent={"center"}>
-                                <Button>
-                                    <EmailIcon />
-                                    <Typography>Contact me</Typography>
-                                </Button>
-                            </Grid>
-                        </Grid>
+                         {/* <Typewriter text="I'm a Software Engineer" delay={120} variant="h2" color="primary.contrastText" /> */}
+                        
+                            <Box mt={4}>
+                                <Grid container spacing={3} display="flex" justifyContent="center">
+                                    <Grid size={{ xs: 10, md: 4 }}>
+                                        <Button>
+                                            <DownloadIcon />
+                                            <Typography>Download CV</Typography>
+                                        </Button>
+                                    </Grid>
+                                    <Grid size={{ xs: 10, md: 4 }}>
+                                        <Button>
+                                            <EmailIcon />
+                                            <Typography>Contact me</Typography>
+                                        </Button>
+                                    </Grid>
+                                </Grid>
+                            </Box>
                     </Grid>
                 </Grid>
             </Container>
-        </StyledHero>
+        </StyledHero >
     )
 }
--- src/styles/heroStyle/styledHero.tsx
+++ src/styles/heroStyle/styledHero.tsx
@@ -5,30 +5,45 @@ export const StyledHero = styled("div")(({ theme }) => ({
     background: theme.palette.primary.main,
     width: "100%",
     height: "100vh",
-    display: "flex",
-    alignItems: "center"
+    border: "1px solid red",
+    [theme.breakpoints.up('xs')]: {
+        display: "block",
+        padding: "20px",
+        paddingTop: "100px",
+        paddingBottom: "40px",
+     
+    },
+    [theme.breakpoints.up('md')]: {
+        display: "flex",
+        alignItems: "center",
+        paddingTop: "100px",
+        height: "100vh"
+    },
+}));
 
-}))
 
-export const StyledImg = styled("img")(({theme}) => ({
+export const StyledImg = styled("img")(({ theme }) => ({
     borderRadius: "50%",
     width: "80%",
     border: `1px solid ${theme.palette.secondary.light}`,
-    
+
 }))
 
 export const StyledButton = styled("button")(({ theme }) => ({
-    gap: 10,
     backgroundColor: "transparent",
-    border: `1px solid ${theme.palette.primary.contrastText}`,
-    borderRadius: "3px",
-    padding: "5px 15px",
-    width: "100%",
     display: "inline-flex",
     alignItems: "center",
     justifyContent: "center",
+    verticalAlign: "middle",
+    gap: "10px",
+    borderRadius: "3px",
+    padding: "5px 15px",
+    width: "100%",
+    border: `1px solid  ${theme.palette.primary.contrastText}`,
     color: theme.palette.primary.contrastText,
+    cursor: "pointer",
+    transition: "background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,border-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
     '&:hover': {
-        backgroundColor: theme.palette.secondary.main
+        backgroundColor: theme.palette.secondary.light,
     }
 }))
